35f76687527a97d239f3bdc28a59d223bf24f908
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-07-04 20:20:21 -0700

    add non-working code on the attempt to login using 'log in with facebook button'

--- app/src/main/java/com/epicodus/sharedchores/SignInActivity.java
+++ app/src/main/java/com/epicodus/sharedchores/SignInActivity.java
@@ -1,13 +1,23 @@
 package com.epicodus.sharedchores;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,10 +26,44 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
-    @Bind(R.id.logIntextView) TextView mLogInTextView;
+    //    LOG IN WITH FACEBOOK
+    private CallbackManager callbackManager;
+    private TextView logInWithFacebookTextView;
+
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            displayMessage(profile);
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+    };
+
+    public SignInActivity() {
+
+    }
+
+//   LOG IN WITH FACEBOOK END
+
     @Bind(R.id.emailSignIn) EditText mEmailSignIn;
     @Bind(R.id.passwordSignIn) EditText mPasswordSignIn;
     @Bind(R.id.signInButton) Button mSignInButton;
+    @Bind(R.id.logInWithFacebookTextView) TextView mLogInWithFacebookTextView;
+    @Bind(R.id.facebookLoginButton) Button mFacebookLoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +78,79 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         mSignInButton = (Button) findViewById(R.id.signInButton);
         Typeface boldieFont = Typeface.createFromAsset(getAssets(), "fonts/Boldie.ttf");
         mSignInButton.setTypeface(boldieFont);
-        mLogInTextView.setTypeface(boldieFont);
+
 // END OF FONTS
 
         mSignInButton.setOnClickListener(this);
+
+//MORE LOGIN WITH FACEBOOK STUFF
+        callbackManager = CallbackManager.Factory.create();
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                displayMessage(newProfile);
+            }
+        };
+
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_sign_in, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.facebookLoginButton);
+        mLogInWithFacebookTextView = (TextView) view.findViewById(R.id.logInWithFacebookTextView);
+
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(callbackManager, callback);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    private void displayMessage(Profile profile) {
+        if (profile != null) {
+            mLogInWithFacebookTextView.setText(profile.getName());
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        displayMessage(profile);
+    }
+
+    //   LOG IN WITH FACEBOOK END
 
     @Override
     public void onClick(View v) {
