9b68daa01e545a49daccdff18b4ac48d4584592f
Fernanda Lowe <fernanda@>
2016-07-12 08:18:59 -0700

    add YelpService class, as well as start building CleaningServicesActivities

--- app/src/main/java/com/epicodus/sharedchores/CleaningServicesActivity.java
+++ app/src/main/java/com/epicodus/sharedchores/CleaningServicesActivity.java
@@ -1,7 +1,16 @@
 package com.epicodus.sharedchores;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+
+        import com.epicodus.sharedchores.services.YelpService;
+
+        import java.io.IOException;
+
+        import okhttp3.Call;
+        import okhttp3.Callback;
+        import okhttp3.Response;
 
 public class CleaningServicesActivity extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class CleaningServicesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cleaning_services);
+    }
+
+    private void getCleaningServices(String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findCleaningServices(location, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v("TAG", jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/epicodus/sharedchores/services/YelpService.java
@@ -0,0 +1,40 @@
+package com.epicodus.sharedchores.services;
+
+import com.epicodus.sharedchores.Constants;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
+/**
+ * Created by Guest on 7/12/16.
+ */
+public class YelpService {
+
+
+    public static void findCleaningServices(String location, Callback callback) {
+
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
+
+        consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new SigningInterceptor(consumer)).build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+}
