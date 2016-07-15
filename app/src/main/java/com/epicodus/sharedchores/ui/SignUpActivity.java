a062a2063844af80238a324f97047c13182fee45
Fernanda Lowe <fe_lowe@hotmail.com>
2016-07-15 13:13:51 -0700

    add log out menu on top left on UserChoreListActivity so user can log out and be redirected to MainActivity

--- app/src/main/java/com/epicodus/sharedchores/ui/SignUpActivity.java
+++ app/src/main/java/com/epicodus/sharedchores/ui/SignUpActivity.java
@@ -2,26 +2,44 @@ package com.epicodus.sharedchores.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.sharedchores.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
-    @Bind(R.id.signUpButton) Button mSignUpButton;
-    @Bind(R.id.createAccountHeader) TextView mCreateAccountHeader;
-    @Bind(R.id.usernameInput) EditText mUsernameInput;
-    @Bind(R.id.emailInput) EditText mEmailInput;
-    @Bind(R.id.passwordInput) EditText mPasswordInput;
-    @Bind(R.id.password2Input) EditText mPassword2Input;
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = SignUpActivity.class.getSimpleName();
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Bind(R.id.createAccountHeader)
+    TextView mCreateAccountHeader;
+    @Bind(R.id.usernameInput)
+    EditText mUsernameInput;
+    @Bind(R.id.emailInput)
+    EditText mEmailInput;
+    @Bind(R.id.passwordInput)
+    EditText mPasswordInput;
+    @Bind(R.id.password2Input)
+    EditText mPassword2Input;
+    @Bind(R.id.signUpButton)
+    Button mSignUpButton;
 
 
     @Override
@@ -30,6 +48,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
 //FONTS EVERYTHING
         Typeface boldieFont = Typeface.createFromAsset(getAssets(), "fonts/Boldie.ttf");
         mCreateAccountHeader.setTypeface(boldieFont);
@@ -37,23 +57,73 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 // END OF FONTS
 
         mSignUpButton.setOnClickListener(this);
+
+        createAuthStateListener();
     }
 
     @Override
     public void onClick(View v) {
         if (v == mSignUpButton) {
-            String usernameInput = mUsernameInput.getText().toString();
-            String emailInput = mEmailInput.getText().toString();
-            String passwordInput = mPasswordInput.getText().toString();
-            String password2Input = mPassword2Input.getText().toString();
 
-            Intent goToWelcomePage = new Intent(this, WelcomePageActivity.class);
-            goToWelcomePage.putExtra("username", usernameInput);
-            goToWelcomePage.putExtra("email", emailInput);
-            goToWelcomePage.putExtra("password", passwordInput);
-            goToWelcomePage.putExtra("password2", password2Input);
-            startActivity(goToWelcomePage);
+            createNewUser();
         }
-
     }
+
+    private void createNewUser() {
+        String username = mUsernameInput.getText().toString().trim();
+        String email = mEmailInput.getText().toString().trim();
+        String password = mPasswordInput.getText().toString().trim();
+        String password2 = mPassword2Input.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        Intent goToWelcomePage = new Intent(this, WelcomePageActivity.class);
+        goToWelcomePage.putExtra("username", username);
+        goToWelcomePage.putExtra("email", email);
+        goToWelcomePage.putExtra("password", password);
+        goToWelcomePage.putExtra("password2", password2);
+        startActivity(goToWelcomePage);
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        if (mAuthListener != null) {
+//            mAuth.removeAuthStateListener(mAuthListener);
+//        }
+//    }
 }
+
--- app/src/main/java/com/epicodus/sharedchores/ui/UserChoreListActivity.java
+++ app/src/main/java/com/epicodus/sharedchores/ui/UserChoreListActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epicodus.sharedchores.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
@@ -80,5 +84,30 @@ public class UserChoreListActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+}
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(UserChoreListActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
