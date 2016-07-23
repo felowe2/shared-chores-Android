7c1aa09ce00f46beaa2964aade59f954af8520d2
Fernanda Lowe <fe_lowe@hotmail.com>
2016-07-22 17:38:48 -0700

    add non-working connection between a friend and a chore

--- app/src/main/java/com/epicodus/sharedchores/adapters/FirebaseFriendListViewHolder.java
+++ app/src/main/java/com/epicodus/sharedchores/adapters/FirebaseFriendListViewHolder.java
@@ -2,19 +2,27 @@ package com.epicodus.sharedchores.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.print.PrintHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.epicodus.sharedchores.Constants;
 import com.epicodus.sharedchores.R;
 import com.epicodus.sharedchores.models.Chore;
 import com.epicodus.sharedchores.models.User;
 import com.epicodus.sharedchores.ui.AssignChoreActivity;
 import com.epicodus.sharedchores.ui.UserChoreDetailActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.parceler.Parcels;
 
 public class FirebaseFriendListViewHolder extends RecyclerView.ViewHolder {
+
+    private User mFriend;
 
     View mView;
     Context mContext;
@@ -35,6 +43,19 @@ public class FirebaseFriendListViewHolder extends RecyclerView.ViewHolder {
         mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//
+//                FirebaseUser firebaseFriend = FirebaseAuth.getInstance().getCurrentUser();
+//                String uid = firebaseFriend.getUid();
+//
+//                DatabaseReference friendRef = FirebaseDatabase
+//                        .getInstance()
+//                        .getReference(Constants.FIREBASE_CHILD_FRIENDS)
+//                        .child(uid);
+//
+//                DatabaseReference pushRef = friendRef.push();
+//                String pushId = pushRef.getKey();
+//                mFriend.setPushId(pushId);
+//                pushRef.setValue(mFriend);
 
                 Intent intent = new Intent(mContext, AssignChoreActivity.class);
                 intent.putExtra("friend", Parcels.wrap(friend));
