8d3a9dc0653c5cdd81894cb1c638ae426a74856a
Fernanda Lowe <fe_lowe@hotmail.com>
2016-07-27 17:07:44 -0700

    refacture user and chores model to try make a connection between users

--- app/src/main/java/com/epicodus/sharedchores/models/Chore.java
+++ app/src/main/java/com/epicodus/sharedchores/models/Chore.java
@@ -1,6 +1,15 @@
 package com.epicodus.sharedchores.models;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
 import org.parceler.Parcel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@IgnoreExtraProperties
 
 @Parcel
 public class Chore {
@@ -9,15 +18,24 @@ public class Chore {
     private String doer;
     private String description;
     private String dueDate;
-    private String pushId;
+    private String timestamp;
+    private String id;
+    private String uid;
 
     public Chore(){}
 
-    public Chore(String title,String doer, String description, String dueDate) {
+    public Chore(String id, String title,String doer, String description, String dueDate, String timestamp) {
+        this.id = id;
         this.title = title;
         this.doer = doer;
         this.description = description;
         this.dueDate = dueDate;
+        this.timestamp = timestamp;
+
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -35,12 +53,38 @@ public class Chore {
     public String getDueDate() {
         return  dueDate;
     }
-
-    public String getPushId() {
-        return pushId;
+    public String getUid() {
+        return uid;
+    }
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+    public String getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+    public void setChore(String id, String title,String doer, String description, String dueDate, String timestamp) {
+        this.id = id;
+        this.title = title;
+        this.doer = doer;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.timestamp = timestamp;
     }
 
-    public void setPushId(String pushId) {
-        this.pushId = pushId;
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("title", title);
+        result.put("doer", doer);
+        result.put("description", description);
+        result.put("dueDate", dueDate);
+        result.put("timestamp", timestamp);
+        result.put("uid", uid);
+
+        return result;
     }
+
 }
--- app/src/main/java/com/epicodus/sharedchores/models/User.java
+++ app/src/main/java/com/epicodus/sharedchores/models/User.java
@@ -1,51 +1,71 @@
 package com.epicodus.sharedchores.models;
 
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.IgnoreExtraProperties;
 
 import org.parceler.Parcel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Parcel
 
 @IgnoreExtraProperties
 public class User {
-    String name;
-    String email;
-    List<String> choreList = new ArrayList<>();
-    //Strin uid;
 
-  private String pushId;
+    private String name;
+    private String email;
+    private String id;
+    private List<String> choreIds = new ArrayList<>();
 
-       public User(){}
+    public User() {
+    }
 
 
-      public User(String name, String email){
+    public User(String id, String name, String email, List<String> choreIds) {
         this.name = name;
-       this.email = email;
-        }
-
-    public List<String> getChoreList() {
-        return choreList;
+        this.email = email;
+        this.id = id;
+        this.choreIds = choreIds;
     }
 
-    public void setChoreList(List<String> choreList) {
-        this.choreList = choreList;
+    public String getName() {
+        return name;
     }
 
-   public String getName() {
-          return name;
-   }
-     public String getEmail(){
-           return email;
-       }
-
-    public String getPushId() {
-           return pushId;
-       }
-
-       public void setPushId(String pushId) {
-      this.pushId = pushId;  }
+    public String getEmail() {
+        return email;
     }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<String> getChoreIds() {
+        return choreIds;
+    }
+    public void setChoreIds(List<String> choreIdList) {
+        this.choreIds = choreIdList;
+    }
+
+    public void addChoreId(String choreId) {
+        this.choreIds.add(choreId);
+    }
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("id", id);
+        result.put("email", email);
+        result.put("choreIds", choreIds);
+
+        return result;
+    }
+
+}
 
